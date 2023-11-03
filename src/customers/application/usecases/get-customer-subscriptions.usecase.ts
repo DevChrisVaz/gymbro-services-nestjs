46651845a83e18afb4592e7e9efc6c6bc3167300
Christian-TICC <christian.vazquez@ticc.com.mx>
2023-11-03 00:58:36 -0600

    add details to customer subscriptions resposne

--- src/customers/application/usecases/get-customer-subscriptions.usecase.ts
+++ src/customers/application/usecases/get-customer-subscriptions.usecase.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { IBranch } from 'src/branches/domain/entities/branch.entity';
 import { DatabaseServicesContract } from 'src/database/domain/contracts/database-services.contract';
+import { IPlan } from 'src/plans/domain/entities/plan.entity';
+import { GetCustomerSubscriptionResponseDto } from 'src/subscriptions/application/dto/responses/get-customer-subscription-response.dto';
 import { ISubscription } from 'src/subscriptions/domain/entities/subscription.entity';
 
 @Injectable()
 export class GetCustomerSubscriptionsUseCase {
-  constructor(private dataServices: DatabaseServicesContract) {}
+  constructor(private dataServices: DatabaseServicesContract) { }
 
-  async run(uuid: string): Promise<ISubscription[]> {
+  async run(uuid: string): Promise<GetCustomerSubscriptionResponseDto[]> {
     const foundSubscriptions: ISubscription[] =
       await this.dataServices.subscriptions.find({ customer: uuid });
-    return foundSubscriptions;
+    const detailedSubscriptions = await Promise.all(
+      foundSubscriptions.map(async subscription => {
+        const plan: IPlan = await this.dataServices.plans.findOne({ uuid: subscription.plan });
+        const branch: IBranch = await this.dataServices.branches.findOne({ uuid: plan.branch });
+        return {
+          uuid: subscription.uuid,
+          status: subscription.status,
+          plan,
+          branch,
+          createdAt: subscription.createdAt,
+          updatedAt: subscription.updatedAt
+        }
+      })
+    )
+    return detailedSubscriptions;
   }
 }
---
+++ src/subscriptions/application/dto/responses/get-customer-subscription-response.dto.ts
@@ -0,0 +1,11 @@
+import { IBranch } from "src/branches/domain/entities/branch.entity";
+import { IPlan } from "src/plans/domain/entities/plan.entity";
+
+export class GetCustomerSubscriptionResponseDto {
+    uuid: string;
+    status: string;
+    plan: IPlan;
+    branch: IBranch;
+    createdAt: string;
+    updatedAt: string;
+}
