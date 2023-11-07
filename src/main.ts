00d847f6b2a035cd3dfdfdab29c127ac9a3f4b18
Christian-TICC <christian.vazquez@ticc.com.mx>
2023-11-06 20:47:38 -0600

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalInterceptors(new TransformInterceptor());
