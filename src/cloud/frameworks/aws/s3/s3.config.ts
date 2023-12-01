b69b474dd7978958159a901d88836956c167559d
Christian Vázquez <85712105+ChrisAlexander0403@users.noreply.github.com>
2023-11-30 20:07:44 -0600

    config updated

--- src/cloud/frameworks/aws/s3/s3.config.ts
+++ src/cloud/frameworks/aws/s3/s3.config.ts
@@ -1,6 +1,6 @@
 export default () => ({
-  s3BucketName: process.env.AWS_BUCKET_NAME,
-  s3BucketRegion: process.env.AWS_BUCKET_REGION,
-  s3PublicKey: process.env.AWS_PUBLIC_KEY,
+  s3BucketName: "gymbro-images",
+  s3BucketRegion: "us-east-2",
+  s3PublicKey: "AKIAW5GOTOSUMSNLWM6V",
   s3PrivateKey: process.env.AWS_SECRET_KEY,
 });
--- src/mailing/frameworks/nodemailer/nodemailer.config.ts
+++ src/mailing/frameworks/nodemailer/nodemailer.config.ts
@@ -1,7 +1,7 @@
 export default () => ({
-  nodemailerEmailService: process.env.NODEMAILER_EMAIL_SERVICE,
-  nodemailerHost: process.env.NODEMAILER_HOST,
-  nodemailerPort: process.env.NODEMAILER_PORT,
+  nodemailerEmailService: "hotmail",
+  nodemailerHost: "smtp.office365.com",
+  nodemailerPort: 587,
   nodemailerUser: process.env.NODEMAILER_USER,
   nodemailerPass: process.env.NODEMAILER_PASSWORD,
 });
